181da9dcf90ac8413e99952db5c3ab3039e2c14b
GiacomoLocatelli <g.locatelli47@studenti.unibg.it>
2023-05-02 15:25:42 +0200

    Update Main.java

--- Gruppo_6/src/Gruppo_6/Main.java
+++ Gruppo_6/src/Gruppo_6/Main.java
@@ -3,9 +3,7 @@ package Gruppo_6;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("\n\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"
-				+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"
-				+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*****\n");
+		
 	    			
 	}
 
