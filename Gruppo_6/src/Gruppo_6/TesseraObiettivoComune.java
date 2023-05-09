078890b24423bfa4ff55e75d4cd735c2fe2babb2
GiacomoLocatelli <g.locatelli47@studenti.unibg.it>
2023-05-09 17:49:24 +0200

    Pulizia e correzione parti di codice, Creazione switch per check commonGoals

--- Gruppo_6/src/Gruppo_6/TesseraObiettivoComune.java
+++ Gruppo_6/src/Gruppo_6/TesseraObiettivoComune.java
@@ -22,18 +22,16 @@ public class TesseraObiettivoComune {
 	    return random2;
 	}
 	
-	public void show_objective() { //mostra l'obiettivo 1 e 2
+	public void show_objective(int a, int b) { //mostra l'obiettivo 1-->a e 2-->b
 		int i=0;
-		int a=0, b=0;
-		a=draw_card();
-		b=draw_card2(a);
+		System.out.println("Obiettivo 1:\n");
 		do {	
 		if(a==b) {
 			System.out.println("Obiettivo 2:\n");
 		}
 		
 	    switch(a) {
-	    case 0: System.out.println("Obiettivo 1:\n");
+	    case 0: 
 	    		System.out.println("Six groups each containing at least\r\n"
 	    		+ "2 tiles of the same type (not necessarily\r\n"
 	    		+ "in the depicted shape).\r\n"
@@ -41,8 +39,9 @@ public class TesseraObiettivoComune {
 	    		+ "from those of another group");
 	    		System.out.println("\n\t*****\n"+"\t*   *\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*   *\n"
 				+"\t*****\n"+"\t*   *\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*   *\n"+"\t*****\n");
+	    		break;
 	    
-	    case 1: System.out.println("Obiettivo 1:\n");
+	    case 1: 
 	    		System.out.println("Four groups each containing at least\r\n"
 	    		+ "4 tiles of the same type (not necessarily\r\n"
 	    		+ "in the depicted shape).\r\n"
@@ -51,63 +50,124 @@ public class TesseraObiettivoComune {
 	    		System.out.println("\n\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"
 				+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"
 				+"\t*****\n"+"\t*   *\n"+"\t* = *\n"+"\t*   *\n"+"\t*****\n");
+	    		break;
 	    		
-	    case 2: System.out.println("Obiettivo 1:\n");
+	    case 2: 
 	    		System.out.println("Four tiles of the same type in the four\r\n"
 	    		+ "corners of the bookshelf.");
-	    
-	    case 3: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 3: 
 	    		System.out.println("Two groups each containing 4 tiles of\r\n"
 	    		+ "the same type in a 2x2 square. The tiles\r\n"
 	    		+ "of one square can be different from\r\n"
 	    		+ "those of the other square.");
-	    
-	    case 4: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 4: 
 	    		System.out.println("Three columns each formed by 6 tiles Five tiles of the same type forming an X.\r\n"
 	    		+ "of maximum three different types. One\r\n"
 	    		+ "column can show the same or a different\r\n"
 	    		+ "combination of another column.");
-	    
-	    case 5: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 5: 
 	    		System.out.println("Eight tiles of the same type. There’s no\r\n"
 	    		+ "restriction about the position of these\r\n"
 	    		+ "tiles.");
-	    
-	    case 6: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 6: 
 	    		System.out.println("Five tiles of the same type forming a\r\n"
 	    		+ "diagonal. ");
-	    
-	    case 7: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 7: 
 	    		System.out.println("Four lines each formed by 5 tiles of\r\n"
 	    		+ "maximum three different types. One\r\n"
 	    		+ "line can show the same or a different\r\n"
 	    		+ "combination of another line.");
-	    
-	    case 8: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 8: 
 	    		System.out.println("Two columns each formed by 6\r\n"
 	    		+ "different types of tiles. ");
-	    
-	    case 9: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 9: 
 	    		System.out.println("Two lines each formed by 5 different\r\n"
 	    		+ "types of tiles. One line can show the\r\n"
 	    		+ "same or a different combination of the\r\n"
 	    		+ "other line.");
-	    
-	    case 10: System.out.println("Obiettivo 1:\n");
+	    		break;
+	    		
+	    case 10: 
 	    		 System.out.println("Five tiles of the same type forming an X.");
-	    
-	    case 11: System.out.println("Obiettivo 1:\n");
+	    		 break;
+	    		 
+	    case 11: 
 	    		 System.out.println("Five columns of increasing or decreasing\r\n"
 	    		+ "height. Starting from the first column on\r\n"
 	    		+ "the left or on the right, each next column\r\n"
 	    		+ "must be made of exactly one more tile.\r\n"
 	    		+ "Tiles can be of any type. ");
+	    		 break;
 	    }
 	    i++;
 	    a=b;
 		}while(i != 2);
 	}
 	
-	
+	public boolean checkCommonGoals(int a, Library libreriaGiocatore[][]) {
+		boolean check=false;
+		
+		switch(a){
+		case 0:
+			
+			break;
+			
+		case 1:
+			break;
+			
+		case 2:
+			if(libreriaGiocatore[0][0]==libreriaGiocatore[0][4] && libreriaGiocatore[0][0]==libreriaGiocatore[5][0] && libreriaGiocatore[0][0]==libreriaGiocatore[5][4]) {
+				check=true;
+			}else {
+				check=false;
+			}
+			break;
+		
+		case 3:
+			break;
+			
+		case 4:
+			break;
+			
+		case 5:
+			
+			break;
+			
+		case 6:
+			break;
+				
+		case 7:
+			break;
+			
+		case 8:
+			break;
+			
+		case 9:
+			break;
+			
+		case 10:
+			break;
+			
+		case 11:
+			break;
+		}
+		
+	return check;
+		
+	}
 
 }
