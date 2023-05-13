0f67f00cf527ec0be701c8300fa0fd7b981ef63c
AndreaFiaccadori <127016033+AndreaFiaccadori@users.noreply.github.com>
2023-05-13 11:05:39 +0200

    Update Library.java
    Adapting the class Library with the class Tiles and renamed variables with the camel case method

--- Library.java
+++ Library.java
@@ -2,12 +2,14 @@ package Gruppo_6;
 import java.util.Scanner;
 
 public class Library {
-	private Object_tile[][] library;
-	private final int rows=6;
-	private final int columns=5;
+	//remember to change all of the variables into private after the checks
+	//remember to change library into Tile after the check
+	protected Tile[][] library;
+	protected final int rows=6;
+	protected final int columns=5;
 	Scanner in=new Scanner(System.in);
-	public Library(Object_tile library) {
-		this.library=new Object_tile[this.rows][this.columns];
+	public Library() {
+		this.library=new Tile[this.rows][this.columns];
 	}
 	//i can't change the value of a constant variable so i don't need setter methods
 	public int getRows() {
@@ -23,21 +25,21 @@ public class Library {
 	 * 3)if the library is all full
 	 */
 	public void libraryFillingChecks() {
-		int column,n_tiles;
+		int column,nTiles;
 		boolean tooMany,full;//I use this two variables to don't call the methods two times
 		//checking the input of the tiles the player wants to put in his library 
 		do {
 			tooMany=false;
 			System.out.println("How many tiles do you want to put in your library?(from 1 to 3)");
-			n_tiles=in.nextInt();
-			if(n_tiles>3||n_tiles<1) 
+			nTiles=in.nextInt();
+			if(nTiles>3||nTiles<1) 
 				System.out.println("You have chosen an invalid value!!");
-			if(tooManyTiles(n_tiles)==true) {
+			if(tooManyTiles(nTiles)==true) {
 				System.out.println("You can't put that many tiles anywhere in your library");
 				tooMany=true;
 			}
 				
-		}while(n_tiles>3||n_tiles<1||tooMany);
+		}while(nTiles>3||nTiles<1||tooMany);
 		//checking if the player can put the tiles in the column he chose 
 		do {
 			full=false;
@@ -45,7 +47,7 @@ public class Library {
 			column=in.nextInt();
 			if(column<1||column>5) 
 				System.out.println("You have chosen a not existing column!!");
-			else if(isColumnFull(n_tiles,--column)==true) {
+			else if(isColumnFull(nTiles,--column)==true) {
 				System.out.println("The column you have chosen is already full or cannot contain that many tiles");
 				full=true;
 			}
@@ -64,15 +66,15 @@ public class Library {
 	 * This method checks if the player can put n tiles in any of the library columns
 	 */
 	public boolean tooManyTiles(int tiles) {
-		int null_counter=0;
+		int nullCounter=0;
 		for(int row=0;row<this.rows;row++) {
 			for(int col=0;col<this.columns;col++) {
 				if(this.library[row][col]==null)
-					null_counter++;
+					nullCounter++;
 			}
-			if(null_counter>tiles)
+			if(nullCounter>tiles)
 				return false;
-			null_counter=0;
+			nullCounter=0;
 		}
 		return true;
 	}
@@ -80,11 +82,11 @@ public class Library {
 	 *The column that the player has chosen is being filled bottom up
 	 */
 	public void libraryFilling(int tiles,int column) {
-		int tiles_put=0;//it counts how many tiles are already been put
-		for(int cont=this.rows-1;tiles_put<tiles;cont--)
+		int tilesPut=0;//it counts how many tiles are already been put
+		for(int cont=this.rows-1;tilesPut<tiles;cont--)
 			if(this.library[cont][column]==null) {
 				//this.Object_tile.[...]==[...]
-				tiles_put++;
+				tilesPut++;
 			}
 	}
 	/**
@@ -98,5 +100,95 @@ public class Library {
 		}
 		return true;
 	}
-	
+	public int score() {
+		int score=0;
+		int sum=0;//it's used to understand from how many points I have to increase the variable score
+		String tile_type;//used to memorize the type of the tile that I find at the start of the cycle 
+		Tile libraryCopy[][]=new Tile[this.rows][this.columns];
+		copyLibrary(libraryCopy);
+		for(int row=0;row<this.rows;row++) { 
+			for(int col=0;col<this.columns;col++) {
+				if(this.library[row][col]!=null) {
+					tile_type=this.library[row][col].getType();
+					this.method(row,col,tile_type);
+					if(sum==3)
+						score+=2;
+					if(sum==3)
+						score+=3;
+					if(sum==3)
+						score+=5;
+					if(sum>=6)
+						score+=8;
+					this.library[row][col]=null;
+					row=0;
+					col=0;
+				}
+			}
+		}
+		return score;
+	}
+	public void copyLibrary(Tile library[][]) {
+		for(int row=0;row<this.rows;row++) 
+			for(int col=0;col<this.columns;col++) 
+				library[row][col]=this.library[row][col];
+
+	}
+	//scrivo dopo
+	public int method(int row,int col,String type) {
+		int sum=1;
+		int adjacentTiles[]=new int[60];
+		int checkedTiles[]=new int[60];
+		int nAdjacent=1,nChecked=0;
+		adjacentTiles[0]=row;
+		adjacentTiles[1]=col;
+		while(nAdjacent!=nChecked) {
+			checkedTiles[nChecked*2]=row;
+			checkedTiles[nChecked*2+1]=col;
+			nChecked++;
+			if(this.library[row-1][col].getType()==type) 
+				if(contains(adjacentTiles,row-1,col)==false) {
+					sum++;
+					adjacentTiles[nAdjacent*2]=row;
+					adjacentTiles[nAdjacent*2+1]=col;
+				}
+			if(this.library[row+1][col].getType()==type) 
+				if(contains(adjacentTiles,row+1,col)==false) {
+					sum++;
+					adjacentTiles[nAdjacent*2]=row;
+					adjacentTiles[nAdjacent*2+1]=col;
+				}
+
+			if(this.library[row][col-1].getType()==type) 
+				if(contains(adjacentTiles,row,col-1)==false) {
+					sum++;
+					adjacentTiles[nAdjacent*2]=row;
+					adjacentTiles[nAdjacent*2+1]=col;
+				}
+
+			if(this.library[row][col+1].getType()==type) 
+				if(contains(adjacentTiles,row,col+1)==false) {
+					sum++;
+					adjacentTiles[nAdjacent*2]=row;
+					adjacentTiles[nAdjacent*2+1]=col;
+				}
+			int val=whichTile(adjacentTiles,checkedTiles);
+			row=val/10;
+			col=val-row;
+		}
+		for(int cont=0;cont<nAdjacent;cont+=2)
+			this.library[cont][cont+1]=null;
+		return sum;
+	}
+	public boolean contains(int array[],int n1,int n2) {
+		for(int cont=0;cont<60;cont+=2)
+			if(array[cont]==n1&&array[cont+1]==n2)
+				return true;
+		return false;
+	}
+	public int whichTile(int a1[],int a2[]) {
+		for(int cont=0;cont<60;cont+=2) 
+			if(a1[cont]!=a2[cont]||a1[cont+1]!=a2[cont+1])
+				return cont*10+(cont+1);
+		return 0;
+	}
 }
