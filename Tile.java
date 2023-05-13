23437dd01abdc575384695f5785b5412bb6d0fa6
Davide Falconi <d.falconi2@studenti.unibg.it>
2023-05-13 16:43:58 +0200

    Modificati nomi dei tipo in Tile

--- Tile.java
+++ Tile.java
@@ -3,7 +3,7 @@ package Gruppo_6;
 import java.util.Random;
 
 public class Tile {
-	private static final String types[]  = {"Cat",   "Book",  "Game",   "Frame", "Trophy", "Plant"};
+	private static final String types[]  = {"cat",   "book",  "game",   "frame", "trophy", "plant"};
 	private static final String colors[] = {"green", "white", "yellow", "blue",  "cyan",   "pink"};
 	private final int type;
 	public Tile() {
