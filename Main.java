47e9cda73e1bed665450677c37dec676cca40ebd
Davide Falconi <d.falconi2@studenti.unibg.it>
2023-05-15 19:22:52 +0200

    Modificata la classe Main per scegliere il numero di giocatori e iniziare la partita

--- Main.java
+++ Main.java
@@ -1,37 +1,22 @@
 package Gruppo_6;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Plank p = new Plank(2);
+		Scanner s = new Scanner(System.in);
 		
-		// Rimozione celle per testare metodo isToFill
-		p.takeTile(2,3);
-		p.takeTile(2,5);
+		int players;
+		System.out.print("Welcome to MyShelfie! How many people will be playing (2-4)? ");
+		players = s.nextInt();
+		while(players<2 || players>4) {
+			System.out.print("There can only be 2, 3 or 4 players. How many people will be playing? ");
+			players = s.nextInt();
+		}
 		
-		p.takeTile(3,3);
-		p.takeTile(3,4);
-		p.takeTile(3,6);
-
-		p.takeTile(4,2);
-		p.takeTile(4,3);
-		p.takeTile(4,5);
-		p.takeTile(4,7);
-
-		p.takeTile(5,2);
-		p.takeTile(5,4);
-		p.takeTile(5,6);
-
-		p.takeTile(6,3);
-		p.takeTile(6,5);
-
-		p.takeTile(7,4);
-		
-		// Rimuovere commenti per far ritornare true
-		//p.takeTile(1,4);
-		//p.takeTile(5,1);
-		System.out.println(p);
-		System.out.println(p.isToFill());
+		Game game = new Game(players);
+		game.start();
 	}
 
 }
