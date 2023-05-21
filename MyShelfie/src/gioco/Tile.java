88d68030c7037dd1d35ea7ea02d8d167f59bb38c
Davide Falconi <d.falconi2@studenti.unibg.it>
2023-05-21 22:08:57 +0200

    Spostato carattere di reset; migliorata la formattazione dei colori

--- MyShelfie/src/gioco/Tile.java
+++ MyShelfie/src/gioco/Tile.java
@@ -3,10 +3,9 @@ package gioco;
 import java.util.Random;
 
 public class Tile {
-	private static final String types[]  = {"cat",   "book",  "game",   "frame", "trophy", "plant"};
-	private static final String colors[] = {"green", "white", "yellow", "blue",  "cyan",   "pink"};
+	private static final String types[]  =     {"cat",        "book",       "game",       "frame",       "trophy",       "plant"};
+	private static final String colors[] =     {"green",      "white",      "yellow",     "blue",        "cyan",         "pink"};
 	private static final String colorCodes[] = {"\033[0;32m", "\033[0;37m", "\033[0;33m", "\033[0;34m",  "\033[0;36m",   "\033[0;95m"};
-	private static final String reset = "\033[0m";
 	private final int type;
 	public Tile() {
 		Random r = new Random();
@@ -25,6 +24,6 @@ public class Tile {
 	}
 
 	public String getFirstTypeChar() {
-		return  colorCodes[type] + types[type].charAt(0) + reset;
+		return  colorCodes[type] + types[type].charAt(0) + "\033[0m";
 	}
 }
