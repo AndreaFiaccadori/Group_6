01e94995cda5f7782d2dae7fa98268dbe78431f9
allypm <50903390+allypm@users.noreply.github.com>
2023-05-23 19:27:19 +0200

    messo insieme classe card e classe PersonalObjective e migliorato alcune parti

--- MyShelfie/src/gioco/PersonalObjective.java
+++ MyShelfie/src/gioco/PersonalObjective.java
@@ -3,7 +3,148 @@ package gioco;
 import java.util.Random;
 
 public class PersonalObjective {
+	
+	final int row=6;
+	final int column=5;
+	String [][] cardGeneral= new String [row][column];
+	
+	/**
+	 * @param numCard numero della carta che si vuole creare
+	 */
+	public PersonalObjective (int numCard) {
+		String [][] card=this.cardGeneral;
+		switch (numCard) {
 		
+			case 0:
+				card [0][4]="blue";
+				card [1][1]="green";
+				card [2][2]="cyan";
+				card [3][0]="pink";
+				card [4][3]="white";
+				card [5][3]="yellow";
+				break;
+				
+			case 1:
+				card [0][0]="pink";
+				card [0][2]="blue";
+				card [1][4]="green";
+				card [2][3]="white";
+				card [3][1]="yellow";
+				card [5][2]="cyan";
+				break;
+		
+			case 2:
+				card [0][0]="green";
+				card [1][3]="blue";
+				card [2][1]="pink";
+				card [3][0]="cyan";
+				card [4][4]="yellow";
+				card [5][2]="white";
+				break;
+				
+			case 3:
+				card [0][2]="cyan";
+				card [0][4]="green";
+				card [2][3]="white";
+				card [4][1]="yellow";
+				card [4][3]="blue";
+				card [5][2]="white";
+				break;
+				
+			case 4:
+				card [0][2]="white";
+				card [1][1]="pink";
+				card [2][2]="blue";
+				card [3][3]="cyan";
+				card [4][4]="yellow";
+				card [5][0]="green";
+				break;
+				
+			case 5:
+				card [0][2]="pink";
+				card [1][1]="white";
+				card [2][0]="yellow";
+				card [3][2]="blue";
+				card [4][4]="green";
+				card [5][3]="cyan";
+				break;
+				
+			case 6:
+				card [1][1]="cyan";
+				card [3][1]="blue";
+				card [3][2]="white";
+				card [4][4]="pink";
+				card [5][0]="yellow";
+				card [5][3]="green";
+				break;
+				
+			case 7:
+				card [0][4]="yellow";
+				card [2][0]="cyan";
+				card [2][2]="blue";
+				card [3][3]="pink";
+				card [4][1]="white";
+				card [4][2]="green";
+				break;
+				
+			case 8:
+				card [0][4]="cyan";
+				card [1][1]="yellow";
+				card [2][0]="white";
+				card [3][3]="green";
+				card [4][1]="blue";
+				card [3][3]="pink";
+				break;
+				
+			case 9:
+				card [0][2]="yellow";
+				card [2][2]="green";
+				card [3][4]="white";
+				card [4][1]="cyan";
+				card [4][4]="pink";
+				card [5][0]="blue";
+				break;
+			
+			case 10:
+				card [1][0]="blue";
+				card [1][3]="yellow";
+				card [2][2]="pink";
+				card [3][1]="green";
+				card [3][4]="cyan";
+				card [5][0]="white";
+				break;
+				
+			case 11:
+				card [1][1]="pink";
+				card [2][0]="green";
+				card [2][2]="yellow";
+				card [3][4]="white";
+				card [4][3]="cyan";
+				card [5][4]="blue";
+				break;
+		}//fine case
+	}// fine costruttore Card
+	
+	/**
+	 * @param row numero di riga
+	 * @param column numero di colonna
+	 * @return ritorna il colore di una cella all'interno della carta obbiettivo personale
+	 */
+	public String getColor(int row, int column) {
+		return String.valueOf(this.cardGeneral[row][column]);
+	}
+	
+	
+	//STAMPARE OBIETTIVO PERSONALE
+	public void printCard () {
+		for (String [] str: this.cardGeneral) {
+			for(String str1: str) {
+				System.out.print(str1+" ");
+			}
+			System.out.println();
+		}
+	}// fine metodo printCard
+	
 	//ESTRAZIONE CARTA
 	/**
 	 * 
@@ -12,13 +153,9 @@ public class PersonalObjective {
 	 */
 	public int [] drawCard (int numPlayer) {
 		int [] extractions=new int [numPlayer];
-
 		int [] cardNotDraw =new int [12];		//array di numeri che posso estrarre
-		for (int cont=0; cont<12; cont++) {
-			cardNotDraw [cont]=cont;
-		}
-
 		Random random = new Random();
+		
 		for (int contExtraction=0; contExtraction<numPlayer; contExtraction++) {
 			int randNum;
 			do{
@@ -30,8 +167,7 @@ public class PersonalObjective {
 		}
 		return extractions;
 	}// fine metodo drawCard
-		
-		
+			
 	//CONTATORE PUNTEGGIO
 	/**
 	 * @param personalLibrary libreria del giocatore
@@ -57,12 +193,6 @@ public class PersonalObjective {
 		}//fine controllo libreria e carta obbiettivo personale
 		//attribuzione punti
 		switch(score) {
-			case 0:
-				break;
-			case 1:
-				break;
-			case 2:
-				break;
 			case 3:
 				score=4;
 				break;
@@ -77,6 +207,5 @@ public class PersonalObjective {
 				break;
 		}
 		return score;
-	}//fine metodo 
+	}//fine metodo playerScore
 }//fine classe
-
